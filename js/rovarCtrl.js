c8a817263286cbf0916bb94a6bc5475e8d1dc1ad
unknown <christofferdahl89@gmail.com>
2014-01-31 11:08:22 +0100

    Commented on earlier unknown pushes

--- js/rovarCtrl.js
+++ js/rovarCtrl.js
@@ -1,9 +1,12 @@
 var rovarApp = angular.module('rovarApp',[]);
 
 rovarApp.controller('rovarCtrl', function($scope) {
+	//Set the params for the text areas
 	$scope.row = 4;
 	$scope.cols = 50;
 	
+	
+	//Converts the text to robber language
 	$scope.convert = function(text) {
 		var kons = "bcdfghjklmnpqrstvwxz";		
 		var textList = text.split("");
